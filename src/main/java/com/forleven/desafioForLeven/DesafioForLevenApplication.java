4f69f73ca31c31690a15fae43b72dd752383a909
Joao Guerra <joaopaulomg_@hotmail.com>
2021-03-09 20:11:58 -0300

    Commit inicial

---
+++ src/main/java/com/forleven/desafioForLeven/DesafioForLevenApplication.java
@@ -0,0 +1,13 @@
+package com.forleven.desafioForLeven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioForLevenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioForLevenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forleven/desafioForLeven/DesafioForLevenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forleven.desafioForLeven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioForLevenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
